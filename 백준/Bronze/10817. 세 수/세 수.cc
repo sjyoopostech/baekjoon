4fef89f2c5cc2393c049aced9640821e1860b678
sjyoopostech <34682715+sjyoopostech@users.noreply.github.com>
2022-03-13 14:55:27 +0900

    [Bronze III] Title: 세 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10817. 세 수/세 수.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+int main() {
+    int a[3];
+    std::cin >> a[0] >> a[1] >> a[2];
+    std::sort(a, a+3);
+    std::cout << a[1];
+
+    return 0;
+}
